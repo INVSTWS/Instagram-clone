6193a35623e8be4a081d9ff07ac325bd03c9a74b
INVSTWS <bizzare9977@gmail.com>
2024-01-23 23:08:38 +0200

    added new attributes to User model: user_name & full_name

---
+++ db/migrate/20240123210400_add_user_name_to_user.rb
@@ -0,0 +1,5 @@
+class AddUserNameToUser < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :user_name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_23_144420) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_23_210458) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_23_144420) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "user_name"
+    t.string "full_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
